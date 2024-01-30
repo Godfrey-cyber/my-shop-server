9281b43b095c3e3c9c54ecc124c410cb0eeca240
Godfrey Ndiritu <godfreyndiritu26@gmail.com>
2024-01-30 10:08:45 +0300

    init of server entry file index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-index.js
+import express from 'express'
+import cors from 'cors'
+import nodemon from 'nodemon'
+import cookieParser from 'cookie-parser'
+
+dotenv.config()
+const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+	origin: process.env.CLIENT_URL,
+	credentials: true
+}))
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Success 💯! server running on port: ${PORT} 👍`)
+})
